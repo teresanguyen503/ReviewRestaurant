0c5bf0dbcc882fdc6a671acc464a8dceb0d4dedd
Teresa Nguyen <nguyentup@gmail.com>
2024-08-29 13:32:53 -0700

    Allow to search by email

--- src/main/java/com/example/restaurantReview/repository/UserRepository.java
+++ src/main/java/com/example/restaurantReview/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.example.restaurantReview.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import com.example.restaurantReview.entity.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-
+    Optional<User> findByEmail(String email);
 }
